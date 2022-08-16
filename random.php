1ef2f368f0bc474dfc0e9d5a667a6d405b041933
BonBonBonifacy <97771278+BonBonBonifacy@users.noreply.github.com>
2022-08-16 11:35:05 +0800

    Update random.php

--- random.php
+++ random.php
@@ -2,7 +2,7 @@
 //初始化随机数生成器种子，这行代码也可以删除
 $seed = time();
 //获取随机数
-$num = mt_rand(0,98);
+$num = mt_rand(0,11);
 //拼接图片地址
 $picpath = "https://cdn.jsdelivr.net/gh/BonBonBonifacy/BonBonBonifacy.github.io@pictures/"$num.".jpg";
 //重定位到图片
